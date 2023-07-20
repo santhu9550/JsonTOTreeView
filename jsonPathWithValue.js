0b4e11897e5251071d11eea2f19cf8fc4464a9a5
Santhosh Gunda <67777176+santhu9550@users.noreply.github.com>
2023-07-20 16:01:17 +0530

    Create jsonPathWithValue.js

---
+++ jsonPathWithValue.js
@@ -0,0 +1,56 @@
+// Import stylesheets
+import './style.css';
+
+function converToTree(inputJson) {
+  function traverse(obj, path) {
+    const result = [];
+    for (const key in obj) {
+      if (typeof obj[key] === 'object') {
+        const label = key;
+        const value = [...path, key];
+        const children = traverse(obj[key], value);
+        result.push({ label, value, children });
+      } else {
+        const label = key;
+        const value = [...path, key];
+        const data = { label, value, result: obj[key] };
+        result.push(data);
+      }
+    }
+    return result;
+  }
+
+  return traverse(inputJson, []);
+}
+
+const inputJson = {
+  id: "0001",
+  type: "donut",
+  name: "Cake",
+  ppu: 0.55,
+  batters: {
+    batter: [
+      { id: "1001", type: "Regular" },
+      { id: "1002", type: "Chocolate" },
+      { id: "1003", type: "Blueberry" },
+      { id: "1004", type: "Devil's Food" },
+    ],
+  },
+  topping: [
+    { id: "5001", type: "None" },
+    { id: "5002", type: "Glazed" },
+    { id: "5005", type: "Sugar" },
+    { id: "5007", type: "Powdered Sugar" },
+    { id: "5006", type: "Chocolate with Sprinkles" },
+    { id: "5003", type: "Chocolate" },
+    { id: "5004", type: "Maple" },
+  ],
+};
+
+const outputJson = converToTree(inputJson);
+console.log(outputJson);
+
+
+// Write Javascript code!
+const appDiv = document.getElementById('app');
+appDiv.innerHTML = `<h1>JS Starter</h1>`;
